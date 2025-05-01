de9e4f033af52e981c3bd746c16a704b8e811fdf
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-05-02 07:41:05 +0800

    Added introductory prompt

--- pythonProject1/number_guessing_game.py
+++ pythonProject1/number_guessing_game.py
@@ -1,1 +1,11 @@
-print("Hello this is the main python file!")
+def numberGuessingGame():
+    print("Welcome to the number guessing game!")
+
+print("Welcome to EspinosaJV's entertaining python game!")
+print("To start off, would you like to play? Y or N")
+userChoice = input()
+
+if userChoice == 'Y':
+    numberGuessingGame()
+else:
+    print("Alright, bye bye!")
